f54291c4a31f26bc25594d93050f6151da85b09e
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-08-13 14:25:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ def view_server(serverID):
 
     return render_template('view_server.html', server=server, discord=_data, **settings)
 
-app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 5000)))
+app.run(debug=True)
